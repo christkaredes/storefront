a00433569aa32e138fcff033600e492ac8cce1a6
karedeschrist <karedeschrist@gmail.com>
2018-03-13 17:33:39 -0400

    Final

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,48 +8,43 @@
 #---
 # encoding: utf-8
 Product.delete_all
-Product.create!(title: 'Rails, Angular, Postgres, and Bootstrap',
+Product.create!(title: 'Lasko 18" Fan',
   description:
     %{<p>
-      <em>Powerful, Effective, and Efficient Full-Stack Web Development</em>
-      As a Rails developer, you care about user experience and performance,
-      but you also want simple and maintainable code. Achieve all that by
-      embracing the full stack of web development, from styling with
-      Bootstrap, building an interactive user interface with AngularJS, to
-      storing data quickly and reliably in PostgreSQL. Take a holistic view of
-      full-stack development to create usable, high-performing applications,
-      and learn to use these technologies effectively in a Ruby on Rails
-      environment.
+      <em>The Lasko Products 18" Pedestal Fan comes with a tilt-back mechanism and height adjustment to let you direct air where you need it. It has an elegant grill and a sleek, space-saving design that will blend well with most any home decor. It is easy to assemble and requires no tools. To ensure utmost safety, this adjustable fan is E.T.L-listed and includes a fused safety plug. 
       </p>},
-  image_url: 'dcbang.jpg',    
+  image_url: 'lasko.jpeg',    
   price: 45.00)
 # . . .
-Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
+Product.create!(title: 'Lg Electronics Front Control Tall-Tub Dishwasher in Stainless Steel with Stainless Steel Tub',
   description:
     %{<p>
-      <em>Native Apps, Multiple Platforms</em>
-      Answer the question “Can we build this for ALL the devices?” with a
-      resounding YES. This book will help you get there with a real-world
-      introduction to seven platforms, whether you’re new to mobile or an
-      experienced developer needing to expand your options. Plus, you’ll find
-      out which cross-platform solution makes the most sense for your needs.
+      <em>LG’s new QuadWash system uses Multi-Motion arms that work together for maximum cleaning coverage, while EasyRack Plus provides a customizable racking experience. LG’s convenient front control design makes it easier than ever to preview and select the settings you want. All this paired with the reassurance of LG’s 10-year Direct Drive Motor warranty. 
       </p>},
-  image_url: '7apps.jpg',
+  image_url: 'lgdishwasher.jpeg',
   price: 26.00)
 # . . .
 
-Product.create!(title: 'Ruby Performance Optimization',
+Product.create!(title: 'Samsung Single Oven Gas Range in Stainless',
   description:
     %{<p>
-      <em>Why Ruby Is Slow, and How to Fix It</em> 
-      You don’t have to accept slow Ruby or Rails performance. In this
-      comprehensive guide to Ruby optimization, you’ll learn how to write
-      faster Ruby code—but that’s just the beginning. See exactly what makes
-      Ruby and Rails code slow, and how to fix it. Alex Dymo will guide you
-      through perils of memory and CPU optimization, profiling, measuring,
-      performance testing, garbage collection, and tuning. You’ll find that
-      all those “hard” things aren’t so difficult after all, and your code
-      will run orders of magnitude faster.
+      <em>Enjoy the flexibility of five burners so you can cook with different-sized pans simultaneously. Whether you're boiling water, melting chocolate or simmering sauce, there's a burner for your needs. The large 5.8 cu. ft. Samsung gas oven allows you to cook a big roast, multiple casserole dishes or several trays of cookies at once. Two oven racks help you prepare enough food for large gatherings, and this range also comes with a convenient storage drawer and a child safety lock. 
       </p>},
-  image_url: 'adrpo.jpg',
+  image_url: 'range.jpg',
+  price: 46.00)
+
+Product.create!(title: 'Whirlpool Over the Range Microwave with True Convection Cooking in Fingerprint Resistant Stainless Steel',
+  description:
+    %{<p>
+      <em>The Whirlpool 1.9 cu. ft. capacity convection microwave with True convection cooking allows you to bake foods right in the microwave. Free up cooktop space by steam cooking foods such as rice, vegetables and fish with this 1100-Watt microwave. You'll have easy microwave cleaning with a clean release non-stick interior. Plus the SteamClean option quickly loosens up spills by using steam without the need for chemicals or cleaners. 
+      </p>},
+  image_url: 'whirlpoolm.jpg',
+  price: 46.00)
+
+Product.create!(title: 'Whirlpool Stainless Steel Bottom-Freezer Refrigerator',
+  description:
+    %{<p>
+      <em>This refrigerator has 5 shelves in it's main storage unit along with LED Interior Lighting for maximum visibility. The FreshFlow™ Produce Preserver protects your vegetables all of the bacteria. An Accu-Chill™ Temperature Management System is what is needed to level out the temperature to make sure your foods are stored in a perfect temperature environment at all times. Designed, Engineered and Assembled in the U.S.A. 
+      </p>},
+  image_url: 'whirlpool.jpg',
   price: 46.00)
